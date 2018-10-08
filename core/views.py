3c4d62ddaf526991cf67bef348157bfd4beef369
Anthony Lopez <tony.lopez.15@gmail.com>
2018-10-08 13:03:11 -0400

    Initial commit

---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def products_list(request):
+    return render(request, 'products_list.html')
+
+
+def product(request):
+    return render(request, 'product.html')
