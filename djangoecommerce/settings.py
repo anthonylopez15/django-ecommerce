510a584b0b546cedb1f2c0888a2e4f106bccf74b
Anthony Lopez <tony.lopez.15@gmail.com>
2018-10-29 19:11:22 -0400

    Add sistema de login/logout

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -143,6 +143,10 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'admin@admin.com.br'
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
+
+
 try:
     from .local_settings import *
 except ImportError:
--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -1,26 +1,15 @@
-"""djangoecommerce URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from core import views
 from catalog import urls as catalog_urls
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('contato/', views.contact, name='contact'),
+    path('entrar/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('sair/', LogoutView.as_view(next_page='/'), name="logout"),
     path('catalogo/', include(catalog_urls)),
     path('admin/', admin.site.urls),
 ]
