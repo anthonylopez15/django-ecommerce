b88b9a2366e50f5f882cc2493b931c00f7688e7b
Anthony Lopez <tony.lopez.15@gmail.com>
2018-10-30 21:04:20 -0400

    add testes al conta

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from core import views
 from catalog import urls as catalog_urls
+from accounts import ulrs as accounts_urls
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
@@ -10,7 +11,8 @@ urlpatterns = [
     path('contato/', views.contact, name='contact'),
     path('entrar/', LoginView.as_view(template_name='login.html'), name="login"),
     path('sair/', LogoutView.as_view(next_page='/'), name="logout"),
-    path('registro/', views.register, name="register"),
+    # path('registro/', views.register, name="register"),
     path('catalogo/', include(catalog_urls)),
+    path('conta/', include(accounts_urls)),
     path('admin/', admin.site.urls),
 ]
