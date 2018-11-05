31850cf5722dd70bf0c1df54c1f8c74cefaed3a8
Anthony Lopez <tony.loprez.15@gmail.com>
2018-11-05 10:12:40 -0400

    adicionando página para o carrinho

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,7 +6,6 @@ from . import views
 app_name = 'checkout'
 
 urlpatterns = [
-    path(
-        'carrinho/adicionar/<slug:slug>/', views.create_cartItem, name='create_cartitem'
-    ),
+    path('carrinho/adicionar/<slug:slug>/', views.create_cartItem, name='create_cartitem'),
+    path('carrinho/', views.cart_item, name='cart_item'),
 ]
