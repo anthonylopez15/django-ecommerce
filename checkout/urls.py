78b6b2991ac674ee56c7bf32167ca6b1cd75d43d
Anthony Lopez <tony.loprez.15@gmail.com>
2018-11-06 16:21:38 -0400

    adicionando página do pedido

--- checkout/urls.py
+++ checkout/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('carrinho/adicionar/<slug:slug>/', views.create_cartItem, name='create_cartitem'),
     path('carrinho/', views.cart_item, name='cart_item'),
     path('finalizando/', views.checkout, name='checkout'),
+    path('meus-pedidos/', views.order_list, name='order_list'),
+    path('meus-pedidos/<int:pk>/', views.order_detail, name='order_detail'),
 ]
